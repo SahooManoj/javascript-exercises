679c85a43068161a5b49eb078e547ef79ab9f3ef
Manoj Sahoo <manoj.sahoo2@ey.com>
2024-09-10 14:56:02 +0530

    javascrip exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(startRange, endRange) {
+    if ((typeof startRange != 'number' || !Number.isInteger(startRange) || startRange < 0) ||
+        (typeof endRange != 'number' || !Number.isInteger(endRange) || endRange < 0)) {
+        return 'ERROR';
+    }
+    if (startRange > endRange) {
+        [startRange, endRange] = [endRange, startRange];
+    }
+    let outputSum = 0;
+    for (i = startRange; i <= endRange; i++) {
+        outputSum += i;
+    }
+    return outputSum
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fDegree) {
+  let cTemp = 0;
+  cTemp = (fDegree - 32) * 5/9 ;
+  return parseFloat(cTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cDegree) {
+  let fTemp = 0;
+  fTemp = (cDegree * 9/5) + 32
+  return parseFloat(fTemp.toFixed(1));
 };
 
 // Do not edit below this line
